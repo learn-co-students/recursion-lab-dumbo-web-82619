3f538fe47c8859ab4e6d2e51b07a37eaf043784f
Karina Macancela <karymacancela@gmail.com>
2019-12-30 22:54:51 -0500

    print each letter recursively

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Code your solution here!
 
+// prints out all of the characters in a passed-in string
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
